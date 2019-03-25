e3ae76ee1cb4d8a4b08676f27723c6a28b74fb01
Arrick <Grapes01@gmail.com>
2019-03-24 20:12:13 -0700

    Commit answred Variable & Datatypes

---
+++ js/app.js
@@ -0,0 +1,28 @@
+//I. Variables & Datatypes
+
+// How do we assign a value to a variable? 
+let random = "value"  // using the operator = you assign things to a variable 
+
+// How do we change the value of a variable?
+random = "newValue"; console.log(random) 
+// using "let" when defining your initial value, you can ressign the value of random with the operator =
+
+// How do we assign an existing variable to a new variable?
+let x = 2; // using a loop and a the while method you can inset a new value into an existing variable
+ while (x < 25) {
+    console.log(x++);
+};
+
+// Remind me, what are declare, assign, and define?
+//declare registers a variable var = 5  where 5 is the declaration //
+//assign provides a value  for a decloration aftter it has been defined
+// define  is the identifer you provide to a variable to assign something to it 
+//var num = 5 where  num would be the definer of the var value of 5 
+
+// What is pseudocoding and why should you do it?
+//pseudocoding are comment notations that you can do to breakdown the steps of your code
+
+// What percentage of time should be spent thinking about how you're 
+//going to solve a problem vs actually typing in code to solve it?
+// 50 % thinkging %50 precent solving 
+
