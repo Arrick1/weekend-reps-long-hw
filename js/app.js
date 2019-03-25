fa9d33d4141d6aa0cd5d6ccc041b7f34a760a842
Arrick <Grapes01@gmail.com>
2019-03-25 07:10:54 -0700

    commit 4 the farm

--- js/app.js
+++ js/app.js
@@ -73,3 +73,22 @@ console.log(e == 'Kevin');
 console.log(a !== b !== c);
 console.log(a === a !== d);
 console.log(48 !== '48');
+
+
+// D. The farm
+
+// Declare a variable animal. Set it to be either "cow" or something else.
+// Write code that will print out "mooooo" if the it is equal to cow.
+// Change your code so that if the variable animal is anything other 
+//than a cow, it will print "Hey! You're not a cow."
+
+var animal = (x)=> {
+    if(x === "cow")
+        console.log("moooo");
+    else if(x !== "cow")
+        console.log("Hey! You're not a cow.");
+}
+animal("cow");
+animal("cat");
+animal("cow")
+
